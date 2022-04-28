7adbb12a86b59341c2adf01f96f05dbe6ed2ad75
Talgat Abdraimov <abdraimov.talga@gmail.com>
2022-04-28 02:54:31 +0000

    Finish generic relationships

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from blog.models import Post, Tag
+from blog.models import Comment, Post, Tag
 
 
 class PostAdmin(admin.ModelAdmin):
@@ -10,3 +10,4 @@ class PostAdmin(admin.ModelAdmin):
 
 admin.site.register(Tag)
 admin.site.register(Post, PostAdmin)
+admin.site.register(Comment)
