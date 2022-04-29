55915e718beb716b9230bd1d81c620c0cb04336d
Talgat Abdraimov <abdraimov.talga@gmail.com>
2022-04-29 17:51:49 +0000

    Finish custom template tags

--- blango/urls.py
+++ blango/urls.py
@@ -5,5 +5,6 @@ from blog import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index)
+    path('', views.index),
+    path("post/<slug>/", views.post_detail, name="blog-post-detail"),
 ]
--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -5,6 +5,8 @@ from django.template import Library
 from django.utils.html import format_html
 from django.utils.safestring import mark_safe
 
+from blog.models import Post
+
 User = get_user_model()
 register = Library()
 
@@ -29,3 +31,29 @@ def author_details(author: Optional[User], current_user):
     suffix = ''
 
   return format_html('{}{}{}', prefix, name, suffix)
+
+
+@register.simple_tag
+def row(extra_classes=""):
+    return format_html('<div class="row {}">', extra_classes)
+
+
+@register.simple_tag
+def endrow():
+    return format_html("</div>")
+
+
+@register.simple_tag
+def col(extra_classes=""):
+    return format_html('<div class="col {}">', extra_classes)
+
+
+@register.simple_tag
+def endcol():
+    return format_html("</div>")
+
+
+@register.inclusion_tag("blog/post-list.html")
+def recent_posts(post):
+    posts = Post.objects.exclude(pk=post.pk)[:5]
+    return {"title": "Recent Posts", "posts": posts}
