3e77ea1d07bff1a824bd973ce173a863a729c09f
Talgat Abdraimov <abdraimov.talga@gmail.com>
2022-05-03 03:44:43 +0000

    Finish logging

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,5 @@
+import logging
+
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 
@@ -5,8 +7,12 @@ from blog.forms import CommentForm
 from blog.models import Post
 
 
+logger = logging.getLogger(__name__)
+
+
 def index(request):
   posts = Post.objects.filter(published_at__lte=timezone.now())
+  logger.debug("Got %d posts", len(posts))
 
   return render(request, 'blog/index.html', {'posts': posts})
 
@@ -21,6 +27,9 @@ def post_detail(request, slug):
               comment.content_object = post
               comment.creator = request.user
               comment.save()
+              logger.info(
+                  "Created comment on Post %d for user %s", post.pk, request.user
+              )
               return redirect(request.path_info)
       else:
           comment_form = CommentForm()
